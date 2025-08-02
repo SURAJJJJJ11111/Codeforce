214d2ef23ab9ff0304459913b11cf87ce2c9f8da
Suraj <surajkr9845@gmail.com>
2025-08-02 23:15:20 +0530

    Add solution for 96A - Football problem
    Added _96A.java and its compiled class file implementing the solution for the Football problem (Codeforces 96A). Updated input.txt and output.txt to reflect a test case and its expected output.

---
+++ _96A.java
@@ -0,0 +1,17 @@
+//	96A - Football
+import java.util.*;
+
+public class _96A {
+    public static void main(String[] args) {
+        Scanner s1=new Scanner(System.in);
+        String s=s1.nextLine();
+        if(s.contains("0000000")||s.contains("1111111"))
+        {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+s1.close();
+    }
+}
