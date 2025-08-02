b9b0a9716f1f1357975a6d3185dda7f49912a5f8
Suraj <surajkr9845@gmail.com>
2025-08-03 03:25:26 +0530

    Fix code formatting in main loop
    Added a space after the opening brace in the while loop for improved code readability.

--- _271A.java
+++ _271A.java
@@ -5,7 +5,7 @@ public class _271A {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         while(true)
-    {
+    { 
         n++;
         String year=String.valueOf(n);
         if(year.charAt(0)!=year.charAt(1)&&year.charAt(0)!=year.charAt(2)&&year.charAt(0)!=year.charAt(3)&&year.charAt(1)!=year.charAt(2)&&year.charAt(1)!=year.charAt(3)&&year.charAt(2)!=year.charAt(3)){
