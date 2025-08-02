2e6b30bbcf8e9a2725beb9a42bc5aacf9781ddde
Suraj <surajkr9845@gmail.com>
2025-08-03 03:24:02 +0530

    Add solution for 'Beautiful Year' problem (271A)
    Added _271A.java and its compiled class file implementing the solution to the 'Beautiful Year' problem. Updated input.txt and output.txt to reflect the new problem statement and expected output.

---
+++ _271A.java
@@ -0,0 +1,17 @@
+// A. Beautiful Year
+import java.util.*;
+public class _271A {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        while(true)
+    {
+        n++;
+        String year=String.valueOf(n);
+        if(year.charAt(0)!=year.charAt(1)&&year.charAt(0)!=year.charAt(2)&&year.charAt(0)!=year.charAt(3)&&year.charAt(1)!=year.charAt(2)&&year.charAt(1)!=year.charAt(3)&&year.charAt(2)!=year.charAt(3)){
+            System.out.println(year);
+            break;
+        }
+    }
+    }
+}
