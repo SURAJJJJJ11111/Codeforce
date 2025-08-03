abe48a9b07119c4066062119dc595d5ff6ce7922
Suraj <surajkr9845@gmail.com>
2025-08-03 15:22:28 +0530

    Add solution for problem 486A Calculating Function
    Added _486A.java implementing the solution for the Calculating Function problem. Updated input.txt and output.txt with new test case and expected output.

---
+++ _486A.java
@@ -0,0 +1,16 @@
+// A. Calculating Function
+import java.util.*;
+
+public class _486A {
+    public static void main(String[] args) {
+        Scanner s1=new Scanner(System.in);
+        long n=s1.nextLong();
+        if(n%2==0){
+            System.out.println(n/2);
+        }else{
+            System.out.println(-(n+1)/2);
+        }
+    }
+    //  
+    
+}
