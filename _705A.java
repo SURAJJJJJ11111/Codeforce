28dd431394450f1325930eda70b7051a22029273
Suraj <surajkr9845@gmail.com>
2025-08-03 16:44:42 +0530

    Add solution for 705A - Hulk problem
    Added _705A.java and its compiled class file implementing the solution for the 705A - Hulk problem. Updated input.txt and output.txt to reflect the new problem and its sample input/output.

---
+++ _705A.java
@@ -0,0 +1,27 @@
+	// 705A - Hulk
+import java.util.*;
+
+public class _705A {
+ public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    int n=scanner.nextInt();
+    StringBuilder sb=new StringBuilder();
+    for(int i=0;i<n;i++){
+        if(i%2!=1)
+        {
+            sb.append("I hate ");
+        }
+        else{
+            sb.append("I love ");
+        }
+        if(i==n-1){
+            sb.append("it ");
+        }
+        else {
+            sb.append("that ");
+        }
+
+    }
+    System.out.println(sb.toString());
+ }   
+}
