1afad2ce1ebbb571ada142c8a24b14552eda7aeb
Suraj <surajkr9845@gmail.com>
2025-08-03 15:33:54 +0530

    Add solution for 1328A - Divisibility Problem
    Added _1328A.java implementing the solution for the Divisibility Problem (Codeforces 1328A). Updated input.txt and output.txt with relevant test cases and expected outputs.

---
+++ _1328A.java
@@ -0,0 +1,20 @@
+	// 1328A - Divisibility Problem
+import java.util.*;
+
+public class _1328A {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        int tt = s1.nextInt();
+        while (tt-- > 0) {
+            long a = s1.nextLong();
+            long b = s1.nextLong();
+            if(a%b==0){
+                System.out.println("0");
+                
+            }
+            else{
+                System.out.println(b-(a%b));
+            }
+        }
+    }
+}
