30b36266e203a43caf07a2afd20bbd445208e011
Suraj <surajkr9845@gmail.com>
2025-08-04 20:11:26 +0530

    Simplify logic in _131A.java main method
    Replaced the conditional logic for cAPS lOCK problem with a simpler approach that always capitalizes the first letter and leaves the rest unchanged. Updated input.txt test case accordingly.

--- _131A.java
+++ _131A.java
@@ -1,40 +1,40 @@
 	// 131A - cAPS lOCK
  	
 //  
-// import java.util.*;
-
-// public class _131A {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         String word = sc.next();
-
-//         String result = Character.toUpperCase(word.charAt(0)) + "" + word.substring(1);
-//         System.out.println(result);
-
-//         sc.close();
-//     }
-// }
-// or 
 import java.util.*;
 
 public class _131A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
-        
-        if (word.equals(word.toUpperCase())) {
-            System.out.println(word.toLowerCase());
-        } else if (
-            Character.isLowerCase(word.charAt(0)) && 
-            word.substring(1).equals(word.substring(1).toUpperCase())
-        ) {
-            System.out.println(
-                Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase()
-            );
-        } else {
-            System.out.println(word);
-        }
+
+        String result = Character.toUpperCase(word.charAt(0)) + "" + word.substring(1);
+        System.out.println(result);
 
         sc.close();
     }
 }
+// or 
+// import java.util.*;
+
+// public class _131A {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String word = sc.next();
+        
+//         if (word.equals(word.toUpperCase())) {
+//             System.out.println(word.toLowerCase());
+//         } else if (
+//             Character.isLowerCase(word.charAt(0)) && 
+//             word.substring(1).equals(word.substring(1).toUpperCase())
+//         ) {
+//             System.out.println(
+//                 Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase()
+//             );
+//         } else {
+//             System.out.println(word);
+//         }
+
+//         sc.close();
+//     }
+// }\
