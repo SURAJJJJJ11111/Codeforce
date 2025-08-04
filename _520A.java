de98d755e20944dc72abed82992cb9af45663454
Suraj <surajkr9845@gmail.com>
2025-08-04 19:27:33 +0530

    Add solution for 520A Pangram problem
    Added _520A.java and its compiled class file implementing a solution to the Pangram problem (520A). Updated input.txt with a test case for the problem.

---
+++ _520A.java
@@ -0,0 +1,25 @@
+
+// 520A - Pangram
+import java.util.*;
+
+public class _520A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String word = sc.next().toLowerCase();
+        Set<Character> alpha = new HashSet<>();
+        for (char ch : word.toCharArray()) {
+            if (ch <= 'z' && ch >= 'a') {
+                alpha.add(ch);
+
+            }
+        }
+        if (alpha.size() == 26) {
+            System.out.println("YES");
+
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+}
