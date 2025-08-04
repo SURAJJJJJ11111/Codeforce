a568b033a2bacd4c03bb19216676ba140d212bc1
Suraj <surajkr9845@gmail.com>
2025-08-04 19:03:47 +0530

    Add solution for Codeforces 208A Dubstep problem
    Added _208a.java and its compiled class file implementing the solution to the Dubstep problem. Updated input.txt and output.txt with relevant test case and expected output.

---
+++ _208a.java
@@ -0,0 +1,14 @@
+// A. Dubstep
+// 
+import java.util.*;
+public class _208a {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String sb=sc.next();
+        String s=sb.replaceAll("WUB"," ");
+        String fs=s.trim();
+        System.out.println(fs.replaceAll(" +"," "));
+       
+    }
+    
+}
