617ac54bf7f308071a7b4e10fb5bdf95fd387c43
Suraj <surajkr9845@gmail.com>
2025-08-06 02:09:31 +0530

    Add solution for 581A - Vasya the Hipster
    Implements a Java program that reads two integers and outputs the maximum number of days Vasya can wear different colored socks and the number of days he can wear same colored socks, solving the 581A problem.

---
+++ _581a.java
@@ -0,0 +1,18 @@
+	// 581A - Vasya the Hipster
+ 	
+ 
+import java.util.*;
+public class _581a {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int fashionday=Math.min(a,b);
+int samecolor=Math.abs(a-b)/2;
+        System.out.println(fashionday+" "+samecolor);
+
+
+
+    }
+    
+}
