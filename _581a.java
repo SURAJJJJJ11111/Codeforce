6fa6b81d101350ad90fd70282797e86007104f7f
Suraj <surajkr9845@gmail.com>
2025-08-06 02:14:14 +0530

    Refactor and clean up _581a.java
    Improved code formatting, added scanner close, and removed unnecessary comments and whitespace for better readability and resource management.

--- _581a.java
+++ _581a.java
@@ -1,18 +1,17 @@
-	// 581A - Vasya the Hipster
- 	
- 
+// 581A - Vasya the Hipster
+
 import java.util.*;
+
 public class _581a {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a =sc.nextInt();
-        int b =sc.nextInt();
-        int fashionday=Math.min(a,b);
-int samecolor=Math.abs(a-b)/2;
-        System.out.println(fashionday+" "+samecolor);
-
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int fashionday = Math.min(a, b);
+        int samecolor = Math.abs(a - b) / 2;
+        System.out.println(fashionday + " " + samecolor);   
+        sc.close();
 
     }
-    
+
 }
