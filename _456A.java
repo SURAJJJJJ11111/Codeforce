adf0e6ba1a1a62b0c6371e0d909f86f1ca3b8a9b
Suraj <surajkr9845@gmail.com>
2025-08-10 03:45:55 +0530

    Add solution for problem 456A in Java
    Implements a program to determine if there exists a laptop with a higher price but lower quality, printing 'Happy Alex' if so, otherwise 'Poor Alex'.

---
+++ _456A.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class _456A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] laptops = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            laptops[i][0] = sc.nextInt();
+            laptops[i][1] = sc.nextInt();
+        }
+        Arrays.sort(laptops, (a, b) -> a[0] - b[0]);
+        for (int i = 0; i < n - 1; i++) {
+            if (laptops[i][1] > laptops[i + 1][1]) {
+                System.out.println("Happy Alex");
+                return;
+            }
+        }
+        System.out.println("Poor Alex");
+    }
+}
