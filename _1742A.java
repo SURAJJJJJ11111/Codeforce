042ac9ab1ea22fd079051120939fced33744904e
Suraj <surajkr9845@gmail.com>
2025-08-11 18:15:13 +0530

    Update _1742A.java

--- _1742A.java
+++ _1742A.java
@@ -9,8 +9,6 @@ public class _1742A {
             int a = sc.nextInt();
             int b = sc.nextInt(); 
             int c = sc.nextInt();
-            
-            
             if (a + b == c || a + c == b || b + c == a) {
                 System.out.println("YES");
             } else {
