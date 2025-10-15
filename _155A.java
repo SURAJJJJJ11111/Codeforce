dbeb9bc407b1b7480ef5bb1ddb8b073c85384151
Suraj <surajkr9845@gmail.com>
2025-10-16 00:43:17 +0530

    Add _155A.java and update _1760A.java formatting
    Added new file _155A.java with initial structure for problem logic. Updated _1760A.java to comment out unused code and improve formatting.

---
+++ _155A.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class _155A {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        int best=0;
+        int worst=0;
+        int amazing=0;
+        for(int)
+
+    }
+}
