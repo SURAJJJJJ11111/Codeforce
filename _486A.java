fad60a8eee1c4c22bcad59d232c6c6877fd92109
Suraj <surajkr9845@gmail.com>
2025-11-28 10:53:01 +0530

    Update _486A.java

--- _486A.java
+++ _486A.java
@@ -5,15 +5,13 @@ public class _486A {
     public static void main(String[] args) {
         Scanner s1=new Scanner(System.in);
         long n=s1.nextLong();
-
-
-        
+ 
         if(n%2==0){
             System.out.println(n/2);
         }else{
             System.out.println(-(n+1)/2);
         }
     }
-    //  
+
     
 }
