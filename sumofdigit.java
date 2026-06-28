cb500c95b28b188930cbdccdab73bec51266e620
Suraj <surajkr9845@gmail.com>
2026-06-28 18:41:24 +0530

    Update sumofdigit.java

--- sumofdigit.java
+++ sumofdigit.java
@@ -2,13 +2,13 @@ import java.util.*;;
 
 public class sumofdigit {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int sum=0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
 
-        while (n>0){
-sum+=n%10;
-n/=10;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
         }
         System.out.println(sum);
     }
