97c02d9c843ee719911bedec7f71ee098da883fc
Suraj <surajkr9845@gmail.com>
2026-06-28 13:32:45 +0530

    asx

---
+++ countsunday.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class countsunday {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String day = sc.nextLine();
+        // String rev= new StringBuilder(day).reverse().toString();
+
+        // if(day.equals(rev)){
+        // System.out.println("palindrome");
+        // }
+        // else {
+        // System.out.println("not plaidrome");
+        // }
+        int date = 0;
+        String days[] = { "mon", "tues", "wedn", "thru", "frid", "satr", "sund" };
+        for (int i = 0; i < days.length; i++) {
+            if (day.equals(days)) {
+                date = i;
+                break;
+            }
+        }
+    }
+}
